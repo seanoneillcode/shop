deaed0e7f69d57e32b33d29a09ce74916823d43c
seanoneillcode <seansaysmeh@gmail.com>
2016-03-20 20:27:37 +0000

    removing last vestigaes of todomvc

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,6 @@ module.exports = {
     module: {
         loaders: [
             {
-                //tell webpack to use jsx-loader for all *.jsx files
                 test: /\.jsx$/,
                 loader: 'jsx-loader?insertPragma=React.DOM&harmony'
             }
