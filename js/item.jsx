689126794ad0fab74857ed08ece5749e01652ac3
seanoneillcode <seansaysmeh@gmail.com>
2016-04-02 18:21:04 +0100

    adding schema rendering

--- js/item.jsx
+++ js/item.jsx
@@ -4,8 +4,59 @@ import React from 'react';
 
 class Item extends React.Component {
 
+  outputSchemaLevel(schema) {
+    for (let key in schema) {
+      console.log("key " + key + " value " + schema[key]);
+    }
+  }
+
+  handleString(schema) {
+    console.log("handleString, ", schema);
+    return "string";
+  }
+
+  handleNumber(schema) {
+    console.log("handleNumber, ", schema);
+    return "number";
+  }
+
+  handleArray(schema) {
+    console.log("handleArray, ", schema);
+    return <div style={{marginLeft: "40px"}}>Array -> {this.decide(schema["items"])}</div>;
+  }
+
+  handleObject(schema) {
+    console.log("handleObject, ", schema);
+    const props = schema["properties"];
+    const list = [];
+    for (let key in props) {
+      list.push(<div style={{marginLeft: "40px"}} key={key}>{key} : {this.decide(props[key])}</div>);
+    }
+    return <div style={{marginLeft: "40px"}}>Object -> {list}</div>;
+  }
+
+  decide(item) {
+    console.log("decide, ", item);
+    const type = item["type"];
+    if (type === "array") {
+      return this.handleArray(item);
+    }
+    if (type === "object") {
+      return this.handleObject(item);
+    }
+    if (type === "string") {
+      return this.handleString(item);
+    }
+    if (type === "number") {
+      return this.handleNumber(item);
+    }
+    return;
+  }
+
   render() {
-    return <div>this is an item, id:{this.props.item.id}</div>
+    const schema = this.props.schema;
+    const renderableSchema = this.decide(schema);
+    return <div>id: {this.props.data.id}, name: {this.props.data.name}, price: {this.props.data.price}, schema:{renderableSchema}</div>
   }
 }
 
--- js/main.jsx
+++ js/main.jsx
@@ -5,12 +5,13 @@ import Item from './item.jsx';
 import React from 'react';
 import ReactDom from 'react-dom';
 import itemData from '../data/products-data.json';
+import itemSchema from '../data/products-schema.json';
 
 class App extends React.Component {
   render() {
     const items = itemData.map((current) => {
-      return <Item item={current} key={current.id}/>
-    });
+      return <Item data={current} schema={itemSchema} key={current.id}/>
+    })[0];
     return <h1><Hello/>, <World/>!
       <div>{items}</div>
     </h1>;
